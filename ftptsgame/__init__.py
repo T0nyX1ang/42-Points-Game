d579874d67b41365633a14726bf618c1c53595d0
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2020-07-18 16:20:37 +0800

    Add a status checker

--- ftptsgame/__init__.py
+++ ftptsgame/__init__.py
@@ -3,7 +3,8 @@
 from .database import DATABASE_42
 from .expr_utils import expr_eval, judge_equivalent
 from .exceptions import (UnmatchedNumberError, WrongAnswerError,
-                         UnsupportedSyntaxError, RepeatedAnswerError)
+                         UnsupportedSyntaxError, RepeatedAnswerError,
+                         GameStatusError)
 import random
 import ast
 
@@ -15,7 +16,7 @@ class GameApp(object):
     Available methods (+ means playing, - means not playing):
     __init__(): initialization. (Entry point)
     is_playing(): show the status of current game. (+-)
-    generate_problem(): generate a problem from database. (+-)
+    generate_problem(): generate a problem from database. (-)
     get_current_problem(): print current problem. (+)
     get_current_solved(): print current solutions. (+)
     get_current_solution_number(): print current solution number. (+)
@@ -31,22 +32,28 @@ class GameApp(object):
         self.__formula = []  # this list stores converted formulas
         self.__playing = False  # this stores playing status
 
-    def is_playing(self):
+    def __status_check(self, required_status=True):
+        """A status checker."""
+        if required_status != self.is_playing():
+            raise GameStatusError(required_status)
+
+    def is_playing(self) -> bool:
         """Incicate the game is started or not."""
         return self.__playing
 
     def generate_problem(self, minimum_solutions: int,
                          maximum_solutions: int) -> tuple:
         """Generate a random problem from the database."""
+        self.__status_check(required_status=False)
         problem_list = [
             k for k in DATABASE_42.keys()
             if minimum_solutions <= DATABASE_42[k] <= maximum_solutions
         ]
-        problem = random.choice(problem_list)
-        return problem
+        self.__problem = random.choice(problem_list)
 
     def get_current_problem(self) -> str:
         """Get current problem. Effective when playing."""
+        self.__status_check(required_status=True)
         message = '本次42点的题目为: %d %d %d %d %d' % (
             self.__problem[0], self.__problem[1], self.__problem[2],
             self.__problem[3], self.__problem[4])
@@ -54,6 +61,7 @@ class GameApp(object):
 
     def get_current_solved(self) -> str:
         """Get current valid solutions. Effective when playing."""
+        self.__status_check(required_status=True)
         line = []
         if len(self.__valid) > 0:
             line.append('有效求解:')
@@ -70,10 +78,12 @@ class GameApp(object):
 
     def get_current_solution_number(self) -> int:
         """Get the number of current solutions. Effective when playing."""
+        self.__status_check(required_status=True)
         return len(self.__valid)
 
     def get_total_solution_number(self) -> int:
         """Get the number of total solutions. Effective when playing."""
+        self.__status_check(required_status=True)
         return DATABASE_42[self.__problem]
 
     def __validate(self, math_expr: str) -> str:
@@ -85,6 +95,7 @@ class GameApp(object):
 
     def solve(self, math_expr: str) -> str:
         """Put forward a solution."""
+        self.__status_check(required_status=True)
         math_expr = math_expr.replace(' ', '').replace('（',
                                                        '(').replace('）', ')')
 
@@ -109,15 +120,14 @@ class GameApp(object):
         self.__formula.append(simplified_expr)
         self.__valid.append(math_expr)
 
-    def start(self, minimum_solutions: int = 1,
-              maximum_solutions: int = 100) -> str:
+    def start(self) -> str:
         """Start the game. Effective when not playing."""
-        self.__problem = self.generate_problem(minimum_solutions,
-                                               maximum_solutions)
+        self.__status_check(required_status=False)
         self.__valid = []
         self.__formula = []
         self.__playing = True
 
     def stop(self) -> str:
         """Stop the game. Effective when playing."""
+        self.__status_check(required_status=True)
         self.__playing = False
