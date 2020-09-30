8eed204017b8ca814fb5e9f3b3e57e2e8f74d6b3
teleport <thss15_jiaozn@163.com>
2020-09-30 09:10:22 +0800

    Fixed equivlent judgment bugs.

--- ftptsgame/expr_utils.py
+++ ftptsgame/expr_utils.py
@@ -2,94 +2,186 @@
 
 import ast
 import math
-import random
+from copy import deepcopy
 from fractions import Fraction
 
 
 class Node(object):
-    """Simple implementation of an expression tree."""
+    """An expression tree."""
 
-    def __init__(self, ch=None, left=None, right=None):
+    NODE_TYPE_NUMBER = 0
+    NODE_TYPE_OPERATOR = 1
+
+    def __init__(self, type=NODE_TYPE_NUMBER, ch=None, left=None, right=None):
         """Initialize the node."""
-        self.ch = ch
+        self.type = type
         self.left = left
         self.right = right
-
-    def __eq__(self, node):
-        """Judge equivalence two expressions."""
-        for _ in range(0, 10):
-            v_test = self.__generate_values(random_value=True)
-            if self.evaluate(v_test) == node.evaluate(v_test):
-                return True
-        return False
-
-    def __ne__(self, node):
-        """Judge in-equivalence two expressions."""
-        return not self.__eq__(node)
-
-    def __pre_simplify_validation(self, value, parent_ch):
-        """Validation before simplication of the formula."""
-        add_sub_check = (value == 0 and parent_ch in '+-')
-        mult_div_check = (abs(value) == 1 and parent_ch in '*/')
-        return add_sub_check or mult_div_check
-
-    def __generate_values(self, random_value=False):
-        """Generate normal or random values."""
-        if random_value:
-            random_number = random.sample(range(500000, 1000000), 14)
-            result = {chr(i + 97): random_number[i] for i in range(0, 14)}
+        if self.type == Node.NODE_TYPE_OPERATOR:
+            self.value = Node.operation(ch, self.left.value, self.right.value)
+            self.ch = ch
         else:
-            result = {chr(i + 97): i for i in range(0, 14)}
-        result['0'], result['1'], result['-1'] = 0, 1, -1  # forcibly replace
-        return result
+            self.value = Fraction(ch)
+            self.ch = '#'
 
-    def evaluate(self, values: dict = {}) -> Fraction:
-        """Evaluate the value of the node using a substitute dictionary."""
-        if not values:
-            values = self.__generate_values()
-
-        operation = {
+    @staticmethod
+    def operation(opt, x, y):
+        if opt == '/' and y == 0:
+            raise ArithmeticError
+        operation_list = {
             '+': lambda x, y: x + y,
             '-': lambda x, y: x - y,
             '*': lambda x, y: x * y,
-            '/': lambda x, y: x / y if y != 0 else math.inf
+            '/': lambda x, y: x / y
         }
+        return operation_list[opt](x, y)
 
-        if self.ch in '+-*/':
-            return operation[self.ch](self.left.evaluate(values),
-                                      self.right.evaluate(values))
+    def unique_id(self):
+        """Return the unique id of this expression.
+           Two expressions have the same tree structure iff they have the same id."""
+        if self.type == Node.NODE_TYPE_OPERATOR:
+            return self.ch + self.left.unique_id() + self.right.unique_id()
         else:
-            return Fraction(values[self.ch])
+            return '[' + str(self.value) + ']'
+
+    def to_string(self):
+        """Return the string form of this expression."""
+        if self.type == Node.NODE_TYPE_OPERATOR:
+            if self.ch in '*/' and self.left.ch in '+-':
+                left_string = '(' + self.left.to_string() + ')'
+            else:
+                left_string = self.left.to_string()
+            if (self.ch in '-*/' and self.right.ch in '+-') or \
+                    (self.ch == '/' and self.right.ch in '*/'):
+                right_string = '(' + self.right.to_string() + ')'
+            else:
+                right_string = self.right.to_string()
+            return left_string + self.ch + right_string
+        else:
+            return str(self.value)
+
+    def evaluate(self, values=None) -> Fraction:
+        """Evaluate the value of this expression using a substitute dictionary."""
+        if values is None:
+            return self.value
+        if self.type == Node.NODE_TYPE_OPERATOR:
+            return Node.operation(self.ch, self.left.evaluate(values),
+                                  self.right.evaluate(values))
+        else:
+            return Fraction(values[int(self.value)])
 
     def extract(self) -> list:
         """Extract numbers from the node."""
-        numbers = []
-        v_normal = self.__generate_values()
-        if self.ch not in '+-*/':
-            numbers.append(v_normal[self.ch])
+        if self.type == Node.NODE_TYPE_OPERATOR:
+            return self.left.extract() + self.right.extract()
         else:
-            numbers = self.left.extract() + self.right.extract()
-        return numbers
+            return [int(self.value)]
 
-    def simplify(self):
-        """Simplify the node."""
-        if self.ch in '+-*/':
-            l_value = self.left.evaluate()
-            r_value = self.right.evaluate()
-            new_node_left = Node(
-                ch=str(l_value)) if self.__pre_simplify_validation(
-                    l_value, self.ch) else self.left.simplify()
-            new_node_right = Node(
-                ch=str(r_value)) if self.__pre_simplify_validation(
-                    r_value, self.ch) else self.right.simplify()
-            new_node = Node(ch=self.ch,
-                            left=new_node_left,
-                            right=new_node_right)
+    def reduce_negative_number(self):
+        """Make all intermediate results of this expression not be negative.
+           The result of whole expression will become its absolute value.
+           This method returns the original result."""
+        if self.type == Node.NODE_TYPE_OPERATOR:
+            left_value = self.left.reduce_negative_number()
+            right_value = self.right.reduce_negative_number()
+            return_value = Node.operation(self.ch, left_value, right_value)
+            if self.ch in '+-':
+                left_opt = 1
+                right_opt = -1 if self.ch == '-' else 1
+                if left_value < 0:
+                    left_value, left_opt = -left_value, -left_opt
+                if right_value < 0:
+                    right_value, right_opt = -right_value, -right_opt
+                if return_value < 0:
+                    left_opt, right_opt = -left_opt, -right_opt
+                if left_opt == 1:
+                    if right_opt == 1:
+                        self.ch = '+'
+                    else:
+                        self.ch = '-'
+                else:
+                    self.ch = '-'
+                    self.left, self.right = self.right, self.left
+            self.value = abs(return_value)
+            return return_value
         else:
-            new_node = Node(ch=self.ch) if self.ch not in 'ab' else Node(
-                ch=str(ord(self.ch) - 97))
+            return self.value
 
-        return new_node
+    def all_equivalent_expression(self, rule_set="Tony"):
+        """Return the list of all equivalent expression of an expression.
+           Don't consider the first rule (equivalence by identical equation).
+           If expression A induces expression B, B may not induce A."""
+        if self.type == Node.NODE_TYPE_OPERATOR:
+            return_list = []
+            left_equal_list = self.left.all_equivalent_expression()
+            right_equal_list = self.right.all_equivalent_expression()
+            left_value, right_value = self.left.value, self.right.value
+            for new_left in left_equal_list:
+                return_list.append(Node(Node.NODE_TYPE_OPERATOR, self.ch, new_left, deepcopy(self.right)))
+            for new_right in right_equal_list:
+                return_list.append(Node(Node.NODE_TYPE_OPERATOR, self.ch, deepcopy(self.left), new_right))
+            # Rule 2: x-0 --> x+0
+            #         x/1 --> x*1
+            #         0/x --> 0*x
+            if self.ch == '-' and right_value == 0:
+                return_list.append(Node(Node.NODE_TYPE_OPERATOR, '+', deepcopy(self.left), deepcopy(self.right)))
+            if self.ch == '/' and right_value == 1:
+                return_list.append(Node(Node.NODE_TYPE_OPERATOR, '*', deepcopy(self.left), deepcopy(self.right)))
+            if self.ch == '/' and left_value == 0:
+                return_list.append(Node(Node.NODE_TYPE_OPERATOR, '*', deepcopy(self.left), deepcopy(self.right)))
+            # Rule 3: (x?y)+0 --> (x+0)?y, x?(y+0)
+            #         (x?y)*1 --> (x*1)?y, x?(y*1)
+            if ((self.ch == '+' and right_value == 0) or
+                (self.ch == '*' and right_value == 1)) \
+                    and self.left.type == Node.NODE_TYPE_OPERATOR:
+                return_list.append(Node(Node.NODE_TYPE_OPERATOR, self.left.ch,
+                                        Node(Node.NODE_TYPE_OPERATOR, self.ch,
+                                             deepcopy(self.left.left),
+                                             deepcopy(self.right)),
+                                        deepcopy(self.left.right))
+                                   )
+                return_list.append(Node(Node.NODE_TYPE_OPERATOR, self.left.ch,
+                                        deepcopy(self.left.left),
+                                        Node(Node.NODE_TYPE_OPERATOR, self.ch,
+                                             deepcopy(self.left.right),
+                                             deepcopy(self.right)))
+                                   )
+            # Rule 4: (y+z)/x --> (x-y)/z, (x-z)/y when x=y+z
+            if self.ch == '/' and self.left.ch == '+' and \
+                    left_value == right_value and \
+                    self.left.left.value != 0 and self.left.right.value != 0:
+                return_list.append(Node(Node.NODE_TYPE_OPERATOR, '/',
+                                        Node(Node.NODE_TYPE_OPERATOR, '-',
+                                             deepcopy(self.right),
+                                             deepcopy(self.left.left)),
+                                        deepcopy(self.left.right))
+                                   )
+                return_list.append(Node(Node.NODE_TYPE_OPERATOR, '/',
+                                        Node(Node.NODE_TYPE_OPERATOR, '-',
+                                             deepcopy(self.right),
+                                             deepcopy(self.left.right)),
+                                        deepcopy(self.left.left))
+                                   )
+            # Rule 5: x*(y/y) --> x+(y-y)
+            if self.ch == '*' and self.right.ch == '/' and right_value == 1:
+                return_list.append(Node(Node.NODE_TYPE_OPERATOR, '+',
+                                        deepcopy(self.left),
+                                        Node(Node.NODE_TYPE_OPERATOR, '-',
+                                             deepcopy(self.right.left),
+                                             deepcopy(self.right.right)))
+                                   )
+            # Rule 6: x_1/x_2 --> x_2/x_1
+            if self.ch == '/' and left_value == right_value:
+                return_list.append(Node(Node.NODE_TYPE_OPERATOR, '/', deepcopy(self.right), deepcopy(self.left)))
+            return return_list
+        else:
+            return []
+
+    def unique_id_for_rule_1(self, values_list):
+        """Return the unique id (for rule 1) of this expression.
+           Two expressions is equivalent by rule 1 iff they have the same id."""
+        results = [self.evaluate(values) for values in values_list]
+        return tuple(results)
 
 
 def _build_node(node):
@@ -101,12 +193,13 @@ def _build_node(node):
         type(ast.Div()): '/'
     }
     if isinstance(node, ast.BinOp) and type(node.op) in node_ref:
-        built_node = Node(ch=node_ref[type(node.op)],
+        built_node = Node(type=Node.NODE_TYPE_OPERATOR,
+                          ch=node_ref[type(node.op)],
                           left=_build_node(node.left),
                           right=_build_node(node.right))
     elif isinstance(node, ast.Num) and type(node.n) is int and node.n in list(
             range(0, 14)):
-        built_node = Node(ch=chr(97 + node.n))
+        built_node = Node(type=Node.NODE_TYPE_NUMBER, ch=node.n)
     else:
         raise SyntaxError('Unallowed operator or operands.')
     return built_node
@@ -115,4 +208,6 @@ def _build_node(node):
 def build_node(token: str) -> Node:
     """Convert a token/string to an AST node."""
     token_ast = ast.parse(token, mode='eval').body
-    return _build_node(token_ast)
+    node = _build_node(token_ast)
+    node.reduce_negative_number()
+    return node
---
+++ ftptsgame/problem_utils.py
@@ -0,0 +1,175 @@
+"""42-points problem utilities for 42 points."""
+
+from expr_utils import Node
+import itertools
+import random
+from copy import deepcopy
+
+import time
+
+
+class Problem(object):
+    """A 42-points problem."""
+
+    def __init__(self, problem):
+        """Initialize the problem."""
+        self.problem = problem
+        self.answer_table = []
+        self.distinct_answer_table = []
+        self.solution_number = -1
+        self.equivalence_dict = {}
+        self.problem.sort()
+
+    @staticmethod
+    def __all_expression_recursive(prob) -> list:
+        """Return the list of all possible expressions of a problem."""
+        n = len(prob)
+        if n == 0:
+            return []
+        if n == 1:
+            return [Node(Node.NODE_TYPE_NUMBER, prob[0])]
+        return_list = []
+        unique_id_set = set()
+        for mask in range(1, 2 ** n - 1):
+            left_prob = []
+            right_prob = []
+            t = mask
+            for i in range(n):
+                if t % 2 == 1:
+                    right_prob.append(prob[i])
+                else:
+                    left_prob.append(prob[i])
+                t //= 2
+            left_set = Problem.__all_expression_recursive(left_prob)
+            right_set = Problem.__all_expression_recursive(right_prob)
+            for left_expr in left_set:
+                for right_expr in right_set:
+                    for opt in '+-*/':
+                        try:
+                            expr = Node(Node.NODE_TYPE_OPERATOR, opt, left_expr, right_expr)
+                            if expr.value < 0:
+                                continue
+                            expr_id = expr.unique_id()
+                            if expr_id not in unique_id_set:
+                                return_list.append(expr)
+                                unique_id_set.add(expr_id)
+                        except ArithmeticError:
+                            pass
+        return return_list
+
+    def __all_expression(self) -> list:
+        """Return the list of all possible expressions of this problem."""
+        return Problem.__all_expression_recursive(self.problem)
+
+    def __all_expression_equals_to_target(self, target=42) -> list:
+        """Return the list of all possible answers of this problem (must be equal to the target)."""
+        return [deepcopy(expr) for expr in self.__all_expression() if expr.evaluate() == target]
+
+    def __all_answer_divided_into_equivalence_classes(self, target=42, max_number=13, rule_set="Tony"):
+        """Divide all answer into some equivalence classes.
+           Return two objects:
+           1. A list including all answers (as expression trees);
+           2. A dictionary, for any answer expression save the representative expression of its class
+              (as the unique id of expressions)."""
+        if rule_set in ['Tony', 'Misaka']:
+            values_list = []
+            for _ in range(10):
+                random_number = random.sample(range(500000, 1000000), max_number + 1)
+                values = {i: random_number[i] for i in range(2, max_number + 1)}
+                values[0] = 0
+                values[1] = 1
+                values_list.append(values)
+            answers = self.__all_expression_equals_to_target(target)
+            parent, rank = {}, {}
+            uid_table, uid_r1_table = {}, {}
+            for expr in answers:
+                uid = expr.unique_id()
+                uid_table[uid] = expr
+                uid_r1 = expr.unique_id_for_rule_1(values_list)
+                if uid_r1 in uid_r1_table:
+                    parent[uid] = uid_r1_table[uid_r1]
+                    rank[uid] = 1
+                else:
+                    parent[uid] = uid
+                    uid_r1_table[uid_r1] = uid
+                    rank[uid] = 2
+
+            def root(uid):
+                nonlocal parent
+                if parent[uid] == uid:
+                    return uid
+                else:
+                    parent[uid] = root(parent[uid])
+                    return parent[uid]
+
+            def union(uid1, uid2):
+                nonlocal parent, rank
+                uid1, uid2 = root(uid1), root(uid2)
+                if uid1 != uid2:
+                    if rank[uid1] <= rank[uid2]:
+                        parent[uid1] = uid2
+                        if rank[uid1] == rank[uid2]:
+                            rank[uid2] += 1
+                    else:
+                        parent[uid2] = uid1
+
+            for expr in answers:
+                uid1 = expr.unique_id()
+                for expr2 in expr.all_equivalent_expression(rule_set):
+                    uid2 = expr2.unique_id()
+                    union(uid1, uid2)
+            return_dict = {}
+            for expr in answers:
+                uid = expr.unique_id()
+                return_dict[uid] = root(uid)
+            return answers, return_dict
+        return [], {}
+
+    def generate_answers(self):
+        """Generate all answers divided into equivalence classes."""
+        self.answer_table, self.equivalence_dict = self.__all_answer_divided_into_equivalence_classes()
+        self.distinct_answer_table = []
+        self.solution_number = 0
+        for expr in self.answer_table:
+            uid = expr.unique_id()
+            if self.equivalence_dict[uid] == uid:
+                self.distinct_answer_table.append(expr)
+                self.solution_number += 1
+
+
+def generate_database():
+    """Update database.py."""
+    problem_size = 5
+    max_number = 13
+    number_pool = list(range(max_number + problem_size))
+    f = open('database.py', 'w')
+    f.write('''"""
+Global database for this project.
+
+This database can be re-generated by command line.
+"""
+
+
+DATABASE_42 = {
+''')
+    first_flag = True
+    for problem_tuple in itertools.combinations(number_pool, problem_size):
+        problem = list(problem_tuple)
+        for i in range(problem_size):
+            problem[i] -= i
+        print(problem, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time())))
+        problem_class = Problem(problem)
+        problem_class.generate_answers()
+        m = len(problem_class.distinct_answer_table)
+        if m == 0:
+            continue
+        if first_flag:
+            f.write('    %s: %d' % (str(tuple(problem)), m))
+        else:
+            f.write(',\n    %s: %d' % (str(tuple(problem)), m))
+        print(problem)
+        first_flag = False
+    f.write('''
+}
+''')
+    f.close()
