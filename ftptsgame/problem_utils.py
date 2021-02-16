2aaa861957a5713300eca7b7441d652e4ca41d4b
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2021-02-16 18:15:18 +0800

    Update rule set

--- ftptsgame/problem_utils.py
+++ ftptsgame/problem_utils.py
@@ -1,11 +1,8 @@
 """42-points problem utilities for 42 points."""
 
 from .expr_utils import Node
-import itertools
 import random
 from copy import deepcopy
-
-import time
 
 
 class Problem(object):
@@ -65,69 +62,67 @@ class Problem(object):
         """Return the list of all possible answers of this problem (must be equal to the target)."""
         return [deepcopy(expr) for expr in self.__all_expression() if expr.evaluate() == target]
 
-    def __all_answer_divided_into_equivalence_classes(self, rule_set="Tony", target=42, max_number=13):
+    def __all_answer_divided_into_equivalence_classes(self, target=42, max_number=13):
         """Divide all answer into some equivalence classes.
            Return two objects:
            1. A list including all answers (as expression trees);
            2. A dictionary, for any answer expression save the representative expression of its class
               (as the unique id of expressions)."""
-        if rule_set in ['Tony', 'Misaka']:
-            values_list = []
-            for _ in range(10):
-                random_number = random.sample(range(500000, 1000000), max_number + 1)
-                values = {i: random_number[i] for i in range(2, max_number + 1)}
-                values[0] = 0
-                values[1] = 1
-                values_list.append(values)
-            answers = self.__all_expression_equals_to_target(target)
-            parent, rank = {}, {}
-            uid_table, uid_r1_table = {}, {}
-            for expr in answers:
-                uid = expr.unique_id()
-                uid_table[uid] = expr
-                uid_r1 = expr.unique_id_for_rule_1(values_list)
-                if uid_r1 in uid_r1_table:
-                    parent[uid] = uid_r1_table[uid_r1]
-                    rank[uid] = 1
+        values_list = []
+        for _ in range(10):
+            random_number = random.sample(range(500000, 1000000), max_number + 1)
+            values = {i: random_number[i] for i in range(2, max_number + 1)}
+            values[0] = 0
+            values[1] = 1
+            values_list.append(values)
+        answers = self.__all_expression_equals_to_target(target)
+        parent, rank = {}, {}
+        uid_table, uid_r1_table = {}, {}
+        for expr in answers:
+            uid = expr.unique_id()
+            uid_table[uid] = expr
+            uid_r1 = expr.unique_id_for_rule_1(values_list)
+            if uid_r1 in uid_r1_table:
+                parent[uid] = uid_r1_table[uid_r1]
+                rank[uid] = 1
+            else:
+                parent[uid] = uid
+                uid_r1_table[uid_r1] = uid
+                rank[uid] = 2
+
+        def root(uid):
+            nonlocal parent
+            if parent[uid] == uid:
+                return uid
+            else:
+                parent[uid] = root(parent[uid])
+                return parent[uid]
+
+        def union(uid1, uid2):
+            nonlocal parent, rank
+            uid1, uid2 = root(uid1), root(uid2)
+            if uid1 != uid2:
+                if rank[uid1] <= rank[uid2]:
+                    parent[uid1] = uid2
+                    if rank[uid1] == rank[uid2]:
+                        rank[uid2] += 1
                 else:
-                    parent[uid] = uid
-                    uid_r1_table[uid_r1] = uid
-                    rank[uid] = 2
+                    parent[uid2] = uid1
 
-            def root(uid):
-                nonlocal parent
-                if parent[uid] == uid:
-                    return uid
-                else:
-                    parent[uid] = root(parent[uid])
-                    return parent[uid]
+        for expr in answers:
+            uid1 = expr.unique_id()
+            for expr2 in expr.all_equivalent_expression():
+                uid2 = expr2.unique_id()
+                union(uid1, uid2)
+        return_dict = {}
+        for expr in answers:
+            uid = expr.unique_id()
+            return_dict[uid] = root(uid)
+        return answers, return_dict
 
-            def union(uid1, uid2):
-                nonlocal parent, rank
-                uid1, uid2 = root(uid1), root(uid2)
-                if uid1 != uid2:
-                    if rank[uid1] <= rank[uid2]:
-                        parent[uid1] = uid2
-                        if rank[uid1] == rank[uid2]:
-                            rank[uid2] += 1
-                    else:
-                        parent[uid2] = uid1
-
-            for expr in answers:
-                uid1 = expr.unique_id()
-                for expr2 in expr.all_equivalent_expression(rule_set):
-                    uid2 = expr2.unique_id()
-                    union(uid1, uid2)
-            return_dict = {}
-            for expr in answers:
-                uid = expr.unique_id()
-                return_dict[uid] = root(uid)
-            return answers, return_dict
-        return [], {}
-
-    def generate_answers(self, rule_set="Tony"):
+    def generate_answers(self):
         """Generate all answers divided into equivalence classes."""
-        self.answer_table, self.equivalence_dict = self.__all_answer_divided_into_equivalence_classes(rule_set)
+        self.answer_table, self.equivalence_dict = self.__all_answer_divided_into_equivalence_classes()
         self.distinct_answer_table = []
         self.solution_number = 0
         for expr in self.answer_table:
